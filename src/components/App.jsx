6cc6d98a4736ec078bf2cabcb87aa2eedf6f5b5e
zaleskiy88 <zaleskiy88@gmail.com>
2024-07-13 17:58:04 +0100

    added displayComponent func & styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,27 @@
-import { AppStyled, MainLayout, InnerLayout } from "../styles/index";
+import { useState } from "react";
+import { AppStyled, MainLayout } from "../styles/index";
+import { Navigation } from "./Navigation/Navigation";
+import { Dashboard, Expenses, Incomes, Transactions } from "./index";
 
 function App() {
+  const [active, setActive] = useState(1);
+
+  const displayComponent = () => {
+    const components = {
+      1: <Dashboard />,
+      2: <Transactions />,
+      3: <Incomes />,
+      4: <Expenses />,
+    };
+
+    return components[active] || <Dashboard />;
+  };
+
   return (
     <AppStyled>
       <MainLayout>
-        <h1>Main Layout</h1>
+        <Navigation active={active} setActive={setActive} />
+        <main>{displayComponent()}</main>
       </MainLayout>
     </AppStyled>
   );
--- src/styles/App.styled.js
+++ src/styles/App.styled.js
@@ -5,4 +5,17 @@ export const AppStyled = styled.div`
   height: 100vh;
   background-image: url(${bg});
   position: relative;
+
+  main {
+    flex: 1;
+    background: var(--main-background);
+    border: 3px solid #ffffff;
+    border-radius: 32px;
+    backdrop-filter: blur(4.5px);
+    overflow: auto;
+    overflow-x: hidden;
+    &::-webkit-scrollbar {
+      width: 0;
+    }
+  }
 `;
