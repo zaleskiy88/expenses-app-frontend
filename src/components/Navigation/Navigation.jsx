3dd9d12e816726d5bf9181312cc82aec7a58d9e8
zaleskiy88 <zaleskiy88@gmail.com>
2024-07-13 17:56:12 +0100

    added navigation list & styles

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,5 +1,51 @@
-import { NavStyled } from "./Navigation.styled";
+import PropTypes from "prop-types";
+import {
+  NavStyled,
+  UserContainer,
+  UserAvatar,
+  NavMenu,
+  MenuItemIcon,
+} from "./Navigation.styled";
+import { menuItems } from "../../utils/menuItems";
+import { FaSignOutAlt } from "react-icons/fa";
 
-export const Navigation = () => {
-  return <NavStyled></NavStyled>;
+import avatar from "../../img/avatar.png";
+
+export const Navigation = ({ active, setActive }) => {
+  return (
+    <NavStyled>
+      <UserContainer>
+        <UserAvatar src={avatar} alt="user´s avatar" />
+        <div>
+          <h2>Username</h2>
+          <p>Your Money</p>
+        </div>
+      </UserContainer>
+
+      <NavMenu>
+        {/* Rendereing menu items */}
+        {menuItems.map((item) => {
+          return (
+            <li
+              key={item.id}
+              onClick={() => setActive(item.id)}
+              className={active === item.id ? "active" : ""}
+            >
+              <MenuItemIcon> {<item.icon />}</MenuItemIcon>
+              <span>{item.title}</span>
+            </li>
+          );
+        })}
+      </NavMenu>
+
+      <div>
+        <FaSignOutAlt /> <span>Sign Out</span>
+      </div>
+    </NavStyled>
+  );
+};
+
+Navigation.propTypes = {
+  active: PropTypes.number.isRequired,
+  setActive: PropTypes.func.isRequired,
 };
--- src/components/Navigation/Navigation.styled.js
+++ src/components/Navigation/Navigation.styled.js
@@ -1,3 +1,77 @@
 import styled from "styled-components";
 
-export const NavStyled = styled.nav``;
+export const NavStyled = styled.nav`
+  padding: 2rem 1.5rem;
+  width: 374px;
+  height: 100%;
+  background: var(--main-background);
+
+  border: 3px solid #ffff;
+  border-radius: 32px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 2rem;
+`;
+
+export const UserContainer = styled.div`
+  height: 100px;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+
+  h2 {
+    color: var(--primary-text-color);
+  }
+`;
+
+export const UserAvatar = styled.img`
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+  object-fit: cover;
+  background: #fcf6f9;
+  border: 2px solid #ffffffff;
+  padding: 0.2rem;
+  box-shadow: 0px 1px 17px rgba(0, 0, 0, 0.06);
+`;
+
+export const NavMenu = styled.ul`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+
+  li {
+    display: grid;
+    grid-template-columns: 40px auto;
+    align-items: center;
+    margin: 0.6rem 0;
+    font-weight: 500;
+    cursor: pointer;
+    transition: var(--transition);
+    padding-left: 1rem;
+    position: relative;
+  }
+
+  .active {
+    color: var(--primary-text-color);
+
+    &::before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 4px;
+      height: 100%;
+      background: var(--primary-text-color);
+      border-radius: 0 10px 10px 0;
+      transition: var(--transition);
+    }
+  }
+`;
+
+export const MenuItemIcon = styled.span`
+  font-size: 1.4rem;
+  transition: var(--transition);
+`;
