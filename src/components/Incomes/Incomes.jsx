4179ad4ec84e98f6e7ea643d6533237a268457c0
zaleskiy88 <zaleskiy88@gmail.com>
2024-07-14 18:16:55 +0100

    added styles

--- src/components/Incomes/Incomes.jsx
+++ src/components/Incomes/Incomes.jsx
@@ -1,15 +1,20 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from "react";
-import { IncomesStyled, IncomeContent, FormContainer, Incomes } from "./Incomes.styled";
+import {
+  IncomesStyled,
+  IncomeContent,
+  FormContainer,
+  Incomes,
+} from "./Incomes.styled";
 import { InnerLayout } from "../../styles/index";
 import { useGlobalContext } from "../../context/useGlobalContext";
-import { IncomesForm } from "../index";
+import { IncomesForm, IncomeItem } from "../index";
 
 export const Income = () => {
   const { incomes, getIncomes, addIncome } = useGlobalContext();
 
   useEffect(() => {
-    console.log(incomes);
+    getIncomes();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
@@ -20,9 +25,45 @@ export const Income = () => {
           <FormContainer>
             <IncomesForm addIncome={addIncome} />
           </FormContainer>
-          <Incomes></Incomes>
+          <Incomes>
+            {incomes.map((income) => {
+              const {
+                _id: id,
+                title,
+                amount,
+                type,
+                date,
+                category,
+                description,
+              } = income;
+
+              return (
+                <IncomeItem
+                  key={id}
+                  title={title}
+                  amount={amount}
+                  date={date}
+                  description={description}
+                />
+              );
+            })}
+          </Incomes>
         </IncomeContent>
       </InnerLayout>
     </IncomesStyled>
   );
 };
+
+// {
+//     title: { type: String, required: true, trim: true, maxLength: 50 },
+//     amount: { type: Number, required: true, trim: true, maxLength: 20 },
+//     type: { type: String, default: "income" },
+//     date: { type: Date, required: true },
+//     category: { type: String, required: true },
+//     description: { type: String, required: true },
+//   }
+{
+  /* <li>
+  <h3>{title}</h3>
+</li>; */
+}
--- src/components/Incomes/Incomes.styled.js
+++ src/components/Incomes/Incomes.styled.js
@@ -2,7 +2,9 @@ import styled from "styled-components";
 
 export const IncomesStyled = styled.div``;
 
-export const IncomeContent = styled.div``;
+export const IncomeContent = styled.div`
+  padding-top: 2rem;
+`;
 
 export const FormContainer = styled.div``;
 
