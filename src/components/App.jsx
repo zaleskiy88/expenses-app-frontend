f949dbe29c80e03abde101605bbe1328240c19f9
zaleskiy88 <zaleskiy88@gmail.com>
2024-07-28 16:43:32 +0100

    added props in <Income/> component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,27 @@
 import { useState } from "react";
-
+import { useGlobalContext } from "../context/useGlobalContext";
 import { AppStyled, MainLayout } from "../styles/index";
 import { Navigation } from "./Navigation/Navigation";
 import { Dashboard, Expenses, Income, Transactions } from "./index";
 
 function App() {
   const [active, setActive] = useState(1);
+  const { incomes, getIncomes, addIncome, deleteIncome, totalIncomes } =
+    useGlobalContext();
 
   const displayComponent = () => {
     const components = {
       1: <Dashboard />,
       2: <Transactions />,
-      3: <Income />,
+      3: (
+        <Income
+          data={incomes}
+          getData={getIncomes}
+          addData={addIncome}
+          deleteData={deleteIncome}
+          totalValue={totalIncomes}
+        />
+      ),
       4: <Expenses />,
     };
 
