ac4b69efa29a03075014e879debfbc320d51ac05
zaleskiy88 <zaleskiy88@gmail.com>
2024-08-05 19:20:01 +0100

    added new Amounts elements to the page

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,12 +1,18 @@
+import { useGlobalContext } from "../../context/useGlobalContext";
 import {
   DashboardStyled,
   StatisticsContainer,
   ChartContainer,
+  AmountsContainer,
+  AmountContainer,
+  TransactionsHistoryContainer,
 } from "./Dashboard.styled";
 import { InnerLayout } from "../../styles/index";
-import { Chart } from "../index";
+import { MdEuro } from "react-icons/md";
+import { Chart, TransactionsHistory } from "../index";
 
 export const Dashboard = () => {
+  const { totalIncomes, totalExpenses, totalBalance } = useGlobalContext();
   return (
     <DashboardStyled>
       <InnerLayout>
@@ -14,7 +20,35 @@ export const Dashboard = () => {
         <StatisticsContainer>
           <ChartContainer>
             <Chart />
+            <AmountsContainer>
+              <AmountContainer>
+                <h2>Total Income</h2>
+                <p>
+                  {<MdEuro />}
+                  {totalIncomes()}
+                </p>
+              </AmountContainer>
+
+              <AmountContainer>
+                <h2>Total Expense</h2>
+                <p>
+                  {<MdEuro />}
+                  {totalExpenses()}
+                </p>
+              </AmountContainer>
+
+              <AmountContainer>
+                <h2>Total Balance</h2>
+                <p>
+                  {<MdEuro />}
+                  {totalBalance()}
+                </p>
+              </AmountContainer>
+            </AmountsContainer>
           </ChartContainer>
+          <TransactionsHistoryContainer>
+            <TransactionsHistory />
+          </TransactionsHistoryContainer>
         </StatisticsContainer>
       </InnerLayout>
     </DashboardStyled>
--- src/components/Dashboard/Dashboard.styled.js
+++ src/components/Dashboard/Dashboard.styled.js
@@ -5,3 +5,9 @@ export const DashboardStyled = styled.div``;
 export const StatisticsContainer = styled.div``;
 
 export const ChartContainer = styled.div``;
+
+export const AmountsContainer = styled.div``;
+
+export const AmountContainer = styled.div``;
+
+export const TransactionsHistoryContainer = styled.div``;
