897279fddce121edd8a271bcb4c1c2abd68cd5de
zaleskiy88 <zaleskiy88@gmail.com>
2024-08-11 19:12:29 +0100

    refactored min-max values visualization; now it show 0 insted of infinity

--- src/components/MinMax/MinMax.jsx
+++ src/components/MinMax/MinMax.jsx
@@ -2,6 +2,9 @@ import PropTypes from "prop-types";
 import { MinMaxTitle, MinMaxValues } from "./MinMax.styled";
 
 export const MinMax = ({ data, title }) => {
+  const min = Math.min(...data.map((item) => item.amount));
+  const max = Math.max(...data.map((item) => item.amount));
+
   return (
     <div>
       <MinMaxTitle>
@@ -9,8 +12,8 @@ export const MinMax = ({ data, title }) => {
       </MinMaxTitle>
 
       <MinMaxValues>
-        <p>€{Math.min(...data.map((item) => item.amount))} </p>
-        <p>€{Math.max(...data.map((item) => item.amount))}</p>
+        <p>€{min > 0 ? min : 0} </p>
+        <p>€{max > 0 ? max : 0}</p>
       </MinMaxValues>
     </div>
   );
