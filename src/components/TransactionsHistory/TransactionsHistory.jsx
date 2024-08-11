c988ea26d840eae1dc36dcf3aca13f9bbd281809
zaleskiy88 <zaleskiy88@gmail.com>
2024-08-11 17:37:18 +0100

    added&styled new component <TransactionsHistory/>

--- src/components/TransactionsHistory/TransactionsHistory.jsx
+++ src/components/TransactionsHistory/TransactionsHistory.jsx
@@ -1,12 +1,30 @@
-import { HistoryStyled } from "./TransactionsHistory.styled";
+import {
+  HistoryStyled,
+  TransactionsList,
+  TransactionsItem,
+} from "./TransactionsHistory.styled";
 import { useGlobalContext } from "../../context/useGlobalContext";
 export const TransactionsHistory = () => {
   const { transactionsHistory } = useGlobalContext();
+  const [...transactions] = transactionsHistory();
 
-  console.log(transactionsHistory());
   return (
     <HistoryStyled>
       <h2>Recent History</h2>
+      <TransactionsList>
+        {transactions ? (
+          transactions.map((item) => (
+            <TransactionsItem key={item._id} $type={item.type}>
+              <p>{item.title}</p>
+              <span>
+                {item.type === "incomes" ? "+" : "-"}€{item.amount}
+              </span>
+            </TransactionsItem>
+          ))
+        ) : (
+          <p>No transactions</p>
+        )}
+      </TransactionsList>
     </HistoryStyled>
   );
 };
--- src/components/TransactionsHistory/TransactionsHistory.styled.js
+++ src/components/TransactionsHistory/TransactionsHistory.styled.js
@@ -1,3 +1,24 @@
 import styled from "styled-components";
 
 export const HistoryStyled = styled.div``;
+
+export const TransactionsList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+export const TransactionsItem = styled.li`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+
+  background: var(--background-color);
+  border: 2px solid #ffffff;
+  border-radius: 20px;
+  box-shadow: var(--shadow);
+
+  color: ${({ $type }) =>
+    $type === "incomes" ? "var(--color-green)" : "var(--color-delete)"};
+`;
