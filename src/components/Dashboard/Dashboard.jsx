a2194d467156ef56f82a7aeb454a991b4cd34086
zaleskiy88 <zaleskiy88@gmail.com>
2024-08-11 17:41:06 +0100

    added <TransactionsHistory/>,<MinMax/> components; Added minor styles update to Total Balance container

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -9,10 +9,11 @@ import {
 } from "./Dashboard.styled";
 import { InnerLayout } from "../../styles/index";
 import { MdEuro } from "react-icons/md";
-import { Chart, TransactionsHistory } from "../index";
+import { Chart, TransactionsHistory, MinMax } from "../index";
 
 export const Dashboard = () => {
-  const { totalIncomes, totalExpenses, totalBalance } = useGlobalContext();
+  const { incomes, expenses, totalIncomes, totalExpenses, totalBalance } =
+    useGlobalContext();
   return (
     <DashboardStyled>
       <InnerLayout>
@@ -20,6 +21,7 @@ export const Dashboard = () => {
         <StatisticsContainer>
           <ChartContainer>
             <Chart />
+
             <AmountsContainer>
               <AmountContainer>
                 <h2>Total Income</h2>
@@ -39,15 +41,27 @@ export const Dashboard = () => {
 
               <AmountContainer>
                 <h2>Total Balance</h2>
-                <p>
+                <p
+                  style={{
+                    color:
+                      totalBalance() > 0
+                        ? "var(--color-green)"
+                        : "var(--color-delete)",
+                  }}
+                >
                   {<MdEuro />}
                   {totalBalance()}
                 </p>
               </AmountContainer>
             </AmountsContainer>
           </ChartContainer>
+
           <TransactionsHistoryContainer>
             <TransactionsHistory />
+
+            <MinMax data={incomes} title={"Incomes"} />
+
+            <MinMax data={expenses} title={"Expenses"} />
           </TransactionsHistoryContainer>
         </StatisticsContainer>
       </InnerLayout>
--- src/components/Dashboard/Dashboard.styled.js
+++ src/components/Dashboard/Dashboard.styled.js
@@ -2,12 +2,48 @@ import styled from "styled-components";
 
 export const DashboardStyled = styled.div``;
 
-export const StatisticsContainer = styled.div``;
+export const StatisticsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  gap: 2rem;
+`;
 
-export const ChartContainer = styled.div``;
+export const ChartContainer = styled.div`
+  grid-column: 1/4;
+`;
 
-export const AmountsContainer = styled.div``;
+export const AmountsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 2rem;
+  margin-top: 2rem;
+`;
 
-export const AmountContainer = styled.div``;
+export const AmountContainer = styled.div`
+  background: var(--background-color);
+  border: 2px solid #ffffff;
+  box-shadow: var(--shadow);
+  padding: 1rem;
+  border-radius: 20px;
 
-export const TransactionsHistoryContainer = styled.div``;
+  &:nth-child(-n + 2) {
+    grid-column: span 2;
+  }
+  &:last-child {
+    grid-column: 2/4;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    opacity: 0.7;
+  }
+
+  p {
+    font-size: 3rem;
+    font-weight: 700;
+  }
+`;
+
+export const TransactionsHistoryContainer = styled.div`
+  grid-column: 4/-1;
+`;
