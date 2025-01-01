524fd8c8cd88f4babe058b8d9ec0547b77d418bc
zaleskiy88 <zaleskiy88@gmail.com>
2025-01-01 17:29:36 +0000

    added chart option selector in dashboard

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,24 +1,17 @@
-import { useEffect } from "react";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Legend,
-  Tooltip,
-  ArcElement,
-} from "chart.js";
-import { Line } from "react-chartjs-2";
+import { useEffect, useState } from "react";
+import { Chart as ChartJS, Legend, Tooltip, ArcElement, plugins } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
 import { ChartStyled } from "./Chart.styled";
 import moment from "moment";
 import { useGlobalContext } from "../../context/useGlobalContext";
 
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Legend, Tooltip, ArcElement);
+ChartJS.register(Legend, Tooltip, ArcElement, plugins);
 
 export const Chart = () => {
-  const { getIncomes, getExpenses, incomes, expenses } = useGlobalContext();
+  const { getIncomes, getExpenses, incomes, expenses, totalIncomes, totalExpenses } = useGlobalContext();
+  const [chartLabels, setChartLabels] = useState(["Incomes", "Expenses"]);
+  const [chartData, setChartData] = useState([totalIncomes(), totalExpenses()]);
+  const [chartBcgColor, setChartBcgColor] = useState(["yellowGreen", "red"]);
 
   useEffect(() => {
     getIncomes();
@@ -26,38 +19,76 @@ export const Chart = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  //Creating array of all dates of incomes and expenses to be used in chart labels
-  const allDates = [...incomes.map((income) => income.date), ...expenses.map((expense) => expense.date)];
-  const formattedDates = allDates
-    .map((date) => {
-      return moment(date).format("DD/M/YYYY");
-    })
-    .sort();
+  useEffect(() => {
+    setChartData([totalIncomes(), totalExpenses()]);
+  }, [incomes, expenses, totalIncomes, totalExpenses]);
+
+  //labels
+  const incomesByCategory = incomes.reduce((acc, income) => {
+    acc[income.category] = (acc[income.category] || 0) + income.amount;
+    return acc;
+  }, {});
+  const expensesByCategory = expenses.reduce((acc, expense) => {
+    acc[expense.category] = (acc[expense.category] || 0) + expense.amount;
+    return acc;
+  }, {});
+  //
+
+  //data
+  const incomesData = [...Object.values(incomesByCategory)];
+  const expensesData = [...Object.values(expensesByCategory)];
+  const incomesAndExpenses = [totalIncomes(), totalExpenses()];
+  //
+
+  //bcg color
+  const incomeBcgColor = Object.keys(incomesData).map((_, index) => {
+    const shade = Math.floor(255 * (index / incomes.length));
+    return `rgba(${0 + shade}, ${120 + shade / 3}, ${200 - shade / 3}, 1)`;
+  });
+  const expenseBcgColor = Object.keys(incomesData).map((_, index) => {
+    const shade = Math.floor(255 * (index / incomes.length));
+    return `rgba(255, ${20 + shade / 2}, ${150 - shade}, 1)`;
+  });
+  //
+
+  //selector handler
+  const selectorHandler = () => (e) => {
+    switch (e.target.value) {
+      case "incomes-vs-expenses":
+        setChartLabels(["Incomes", "Expenses"]);
+        setChartData(incomesAndExpenses);
+        setChartBcgColor(["yellowGreen", "red"]);
+        break;
+
+      case "incomes":
+        setChartLabels(Object.keys(incomesByCategory));
+        setChartData(incomesData);
+        setChartBcgColor(incomeBcgColor);
+        break;
+
+      case "expenses":
+        setChartLabels(Object.keys(expensesByCategory));
+        setChartData(expensesData);
+        setChartBcgColor(expenseBcgColor);
+        break;
+
+      default:
+        setChartLabels(["Incomes", "Expenses"]);
+        setChartData(incomesAndExpenses);
+        setChartBcgColor(["yellowGreen", "red"]);
+        break;
+    }
+  };
+
+  //
 
   const data = {
-    labels: formattedDates,
+    labels: chartLabels,
 
     datasets: [
       {
-        label: "Incomes",
-        data: [
-          ...incomes.map((income) => {
-            const { amount } = income;
-            return amount;
-          }),
-        ],
-        backgroundColor: "#42AD00",
-        tension: 0.2,
-      },
-      {
-        label: "Expenses",
-        data: [
-          ...expenses.map((expense) => {
-            const { amount } = expense;
-            return amount;
-          }),
-        ],
-        backgroundColor: "#FF0000",
+        data: chartData,
+        backgroundColor: chartBcgColor,
         tension: 0.2,
       },
     ],
@@ -66,11 +97,29 @@ export const Chart = () => {
   const chartOptions = {
     maintainAspectRatio: true,
     responsive: true,
+    plugins: {
+      tooltip: {
+        callbacks: {
+          label: function (context) {
+            const label = context.label || "";
+            const value = context.raw || 0;
+            return `${label}: $${value.toFixed(2)}`;
+          },
+        },
+      },
+    },
   };
 
   return (
     <ChartStyled>
-      <Line data={data} options={chartOptions} style={{ display: "inline-block" }} />
+      {/*chart selector*/}
+      <select onChange={selectorHandler()}>
+        <option value="incomes-vs-expenses">Incomes Vs. Expenses</option>
+        <option value="incomes">Incomes</option>
+        <option value="expenses">Expenses</option>
+      </select>
+
+      <Doughnut data={data} options={chartOptions} style={{ display: "inline-block" }} />
     </ChartStyled>
   );
 };
