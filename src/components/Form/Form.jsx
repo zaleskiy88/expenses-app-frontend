a89594ff6f98264623c7a2a076fb9e00e27b144e
zaleskiy88 <zaleskiy88@gmail.com>
2025-01-06 13:46:18 +0000

    removed type property in formState

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -46,7 +46,6 @@ export const Form = ({ formType, formHandler }) => {
     setFormState({
       title: "",
       amount: "",
-      type: formType,
       date: new Date(),
       category: "",
       description: "",
